3e2499e0032dfb2090e3245832d4be957c07bc6d
Aditya Raj Singh Ranawat <adityarajsinghranawatjobs@gmail.com>
2025-05-04 09:46:41 +0530

    updating icon

--- script.js
+++ script.js
@@ -1,70 +1,58 @@
 const accessKey = "WZikcYQ5KUL-4RklPLj15Y_A9kNprOB_3P8ga8Lax5A";
 
-
-
-
 const formE1 = document.querySelector("form");
 const inputE1 = document.getElementById("search-input");
 const searchResults = document.querySelector(".search-results");
 const showMore = document.getElementById("show-more-button");
 
-
-
 let inputData = "";
 let page = 1;
 
 async function searchImages() {
-    inputData = inputE1.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+  inputData = inputE1.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
 
-    const response = await fetch(url);  // feth response
-    const data = await response.json(); // conver data to json
+  const response = await fetch(url); // feth response
+  const data = await response.json(); // conver data to json
 
-    const results = data.results;
+  const results = data.results;
 
+  if (page === 1) {
+    searchResults.innerHTML = "";
+  }
 
-    if(page === 1){
-        searchResults.innerHTML = "";
-    }
+  results.map((result) => {
+    const imageWrapper = document.createElement("div");
+    imageWrapper.classList.add("search-result"); //add css result
 
-    results.map((result) => {
-        const imageWrapper = document.createElement("div");
-        imageWrapper.classList.add("search-result");
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+    image.alt = result.alt_description;
 
-        const image = document.createElement("img");
-        image.src = result.urls.small;
-        image.alt = result.alt_description;
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+    imageLink.textContent = result.alt_description;
 
-        const imageLink = document.createElement("a");
-        imageLink.href = result.links.html;
-        imageLink.target = "_blank";
-        imageLink.textContent = result.alt_description;
+    imageWrapper.appendChild(image);
+    imageWrapper.appendChild(imageLink);
 
-        
-        imageWrapper.appendChild(image);
-        imageWrapper.appendChild(imageLink);
-        searchResults.appendChild(imageWrapper);
+    searchResults.appendChild(imageWrapper);
+  });
 
+  page++;
 
-    })
-
-
-    page++;
-
-    if(page > 1){
-        showMore.style.display = "block";
-    }
+  if (page > 1) {
+    showMore.style.display = "block";
+  }
 }
 
 formE1.addEventListener("submit", (event) => {
-    event.preventDefault();
-    page = 1;
-    searchImages();2
+  event.preventDefault();
+  page = 1;
+  searchImages();
 });
 
 showMore.addEventListener("click", () => {
-    searchImages();
+  searchImages();
 });
-
-
-
